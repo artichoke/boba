a2bfa2d79c91f2d84239b5cc68d127cb32cf3541
ci-build <ci-build@hyperbo.la>
2019-11-26 19:52:05 +0000

    [skip ci] build docs

---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["bubblebabble"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"bubblebabble/enum.DecodeError.html\" title=\"enum bubblebabble::DecodeError\">DecodeError</a>",synthetic:false,types:["bubblebabble::DecodeError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
