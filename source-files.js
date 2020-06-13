e2eeea25dfc70e7f020535c38d4a18d51dc4c63a
artichoke-ci <ci@artichokeruby.org>
2020-06-13 22:20:43 +0000

    deploy: 758839e5e7a5ccf42b05fabad3b4f69ce75c2e9b

--- source-files.js
+++ source-files.js
@@ -1,3 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["boba"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["bstr"] = {"name":"","dirs":[{"name":"byteset","files":["mod.rs","scalar.rs"]},{"name":"search","files":["byte_frequencies.rs","mod.rs","prefilter.rs","twoway.rs"]}],"files":["ascii.rs","bstr.rs","bstring.rs","cow.rs","ext_slice.rs","ext_vec.rs","impls.rs","io.rs","lib.rs","utf8.rs"]};
+sourcesIndex["memchr"] = {"name":"","dirs":[{"name":"x86","files":["avx.rs","mod.rs","sse2.rs"]}],"files":["fallback.rs","iter.rs","lib.rs","naive.rs"]};
 createSourceSidebar();
